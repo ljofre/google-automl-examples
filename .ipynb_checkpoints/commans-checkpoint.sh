b8aadb6e4474a918bcd5cff3d8f227506cb8c2cc
Leonardo Andrés Jofré Flor <ljofre2146@gmail.com>
2018-08-02 18:56:41 -0400

    Se agrega tomate cherry a los datos de entrenamiento, descargando imagenes directamente de google images

---
+++ .ipynb_checkpoints/commans-checkpoint.sh
@@ -0,0 +1,20 @@
+# copiamos la fuente de datos desde nuestro proyecto en google cloud
+
+#wget http://data.vision.ee.ethz.ch/cvl/food-101.tar.gz
+gsutil cp gs://food101latam/food-101.tar.gz .
+
+# descomprimir el contenido sin verbosidad para mayor velocidad
+tar xzf food-101.tar.gz
+# tar xzvf food-101.tar.gz verboso
+#sudo mount -o discard,defaults /dev/sdb  ~/disks/food-101-disk-dataset
+
+
+
+!gsutil cp ./food_target.csv gs://food101latam/food-101/
+
+
+## listar los archivos dentro del bucket
+datalab create --no-create-repository --machine-type n1-highmem-2 image-class2
+datalab connect image-class2
+
+## descargar archivos
